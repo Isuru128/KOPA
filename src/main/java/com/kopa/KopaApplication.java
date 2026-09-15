8288243397495d16ba825963cdadfbd23144bc34
Isuru128 <isurudula28@gmail.com>
2026-09-15 23:49:39 +0530

    implement core backend architecture and frontend modules for reservation, menu, and auth

--- src/main/java/com/kopa/KopaApplication.java
+++ src/main/java/com/kopa/KopaApplication.java
@@ -3,7 +3,10 @@ package com.kopa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(excludeName = {
+    "org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration",
+    "org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration"
+})
 public class KopaApplication {
 
     public static void main(String[] args) {
