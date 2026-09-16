db6799ede13d7a528c8efdc12071f37046db96df
Isuru128 <isurudula28@gmail.com>
2026-09-16 22:46:58 +0530

    add initial core application components including models, security config, and web controller

--- src/main/java/com/kopa/model/TableEntity.java
+++ src/main/java/com/kopa/model/TableEntity.java
@@ -4,14 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "tables")
 public class TableEntity {
+    @Id
     private String id;
+    
+    @Indexed(unique = true)
     private String tableNumber;
+    
     private int capacity;
     private String type; // Window Table, Couple Table, Standard Table, Group Table, Outdoor Table, Counter Seat
     private String location; // Window Bay, Main Hall, Bar Counter, Garden Terrace
