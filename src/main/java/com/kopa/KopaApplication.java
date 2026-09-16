833d147437d802a8135d9b8797801b537c9779ec
Isuru128 <isurudula28@gmail.com>
2026-09-16 22:53:58 +0530

    feat: add Spring Boot application main class

--- src/main/java/com/kopa/KopaApplication.java
+++ src/main/java/com/kopa/KopaApplication.java
@@ -3,10 +3,7 @@ package com.kopa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(excludeName = {
-    "org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration",
-    "org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration"
-})
+@SpringBootApplication
 public class KopaApplication {
 
     public static void main(String[] args) {
