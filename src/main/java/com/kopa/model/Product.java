f874a03cb766a05281c0334bbe016bfa9032f86d
Isuru128 <isurudula28@gmail.com>
2026-09-16 22:43:35 +0530

    feat: initialize Spring Boot application structure with domain models, repositories, services, and UI template

--- src/main/java/com/kopa/model/Product.java
+++ src/main/java/com/kopa/model/Product.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
@@ -11,11 +14,17 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "products")
 public class Product {
+    @Id
     private String id;
+    
     private String name;
     private String description;
+    
+    @Indexed
     private String category;
+    
     private double price;
     private String image;
     private boolean available;
